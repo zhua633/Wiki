8aaed8ed85e0aa8bb4a10924ead5648f34369d3b
zhua633 <68888761+zhua633@users.noreply.github.com>
2021-09-19 14:14:02 +1200

    Add files via upload
    Added new page, random page functions

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("item/<str:entry>", views.item, name="item"),
-    path("NoEntry", views.NoEntry, name="NoEntry")
-
+    path("NoEntry", views.NoEntry, name="NoEntry"),
+    path("searchresults",views.searchresults,name="searchresults"),
+    path("NewPage", views.NewPage, name="NewPage"),
+    path("RandomPage",views.RandomPage,name="RandomPage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
 from markdown2 import Markdown
 from . import util
+from random import randint
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+
+
+
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(label="Content")
+
+def NewPage(request):
+    if request.method == "POST":
+
+        form = NewPageForm(request.POST)
+
+        if form.is_valid():
+
+            title= form.cleaned_data["title"]
+            content= form.cleaned_data["content"]
+
+            current_entries=util.list_entries()
+            for entry in current_entries:
+                if entry.lower() == title.lower():
+                    messages.warning(request, 'This entry already exists, please choose another title')
+                    return render(request,"encyclopedia/NewPage.html", {"form": form})
+
+            util.save_entry(title,content)
+            with open(f"entries/{title}.md", "w") as file:
+                hash="# "+title+"\n"
+                file.write(hash)
+                file.write(content)
+
+            content = util.get_entry(title)
+            return render(request,"encyclopedia/item.html",
+                {"title":title, "entry": Markdown().convert(content)}
+            )
+
+        else:
+            return render(request,"encyclopedia/NewPage.html", {"form": form})
+    return render(request, "encyclopedia/NewPage.html", {"form": NewPageForm()})
+
+
 
 
 def index(request):
@@ -20,3 +64,30 @@ def item(request, entry):
         return render(request,"encyclopedia/item.html",
             {"title": entry, "entry": Markdown().convert(content)},
         )
+
+def searchresults(request):
+    
+    querystr=request.GET.get('q','')
+    results=[]
+
+    if util.get_entry(querystr) is None:
+        for entry in util.list_entries():
+            if querystr.lower() in entry.lower():
+                results.append(entry)
+        return render(request,"encyclopedia/searchresults.html", {"results": results, "querystr": querystr})
+
+    else: 
+        content = util.get_entry(querystr)
+        return render(request,"encyclopedia/item.html",
+            {"title":querystr, "entry": Markdown().convert(content)}
+        )
+
+def RandomPage(request):
+    pages = util.list_entries()
+    entry=pages[randint(0,len(pages)-1)]
+
+    return render(request,"encyclopedia/item.html",
+        {"title":entry, "entry": Markdown().convert(util.get_entry(entry))}
+    )
+            
+    
